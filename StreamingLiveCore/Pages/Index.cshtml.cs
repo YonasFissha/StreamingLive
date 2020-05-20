4701639fffadfdc25d57c49f0162150bdaafb7c1
Jeremy Zongker <jeremyzongker@livechurchsolutions.org>
2020-05-19 22:31:39 -0500

    Started a .NET core version of StreamingLiveWeb

---
+++ StreamingLiveCore/Pages/Index.cshtml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace StreamingLiveCore.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public string Title = "Hello World";
+
+        public void OnGet()
+        {
+            int a = 0;
+        }
+
+        public void OnPost()
+        {
+            int a = 0;
+        }
+
+        
+        public void OnPostTest()
+        {
+            int b = 0;
+        }
+
+
+
+
+    }
+}
