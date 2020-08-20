9396768fd56aabc7545c49216cf942df3941bbb3
Jeremy Zongker <jeremyzongker@livechurchsolutions.org>
2020-08-19 21:52:10 -0500

    Removed dist and updated gitignore

--- StreamingLiveApi/dist/constants/index.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var types_1 = require("./types");
-Object.defineProperty(exports, "TYPES", { enumerable: true, get: function () { return types_1.TYPES; } });
-//# sourceMappingURL=index.js.map
--- StreamingLiveApi/dist/helpers/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var ControllerHelper_1 = require("./ControllerHelper");
-Object.defineProperty(exports, "ControllerHelper", { enumerable: true, get: function () { return ControllerHelper_1.ControllerHelper; } });
-var Utils_1 = require("./Utils");
-Object.defineProperty(exports, "Utils", { enumerable: true, get: function () { return Utils_1.Utils; } });
-//# sourceMappingURL=index.js.map
