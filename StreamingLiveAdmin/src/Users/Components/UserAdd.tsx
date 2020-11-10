54aa490444ae0d207485268542cddfbd8da28ca9
Jeremy Zongker <jeremyzongker@livechurchsolutions.org>
2020-11-10 01:09:09 -0600

    Email users when they are granted access.

--- StreamingLiveAdmin/src/Users/Components/UserAdd.tsx
+++ StreamingLiveAdmin/src/Users/Components/UserAdd.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { InputBox, RoleInterface } from '.'
 import { FormGroup } from 'react-bootstrap';
-import { ApiHelper, UserInterface, RoleMemberInterface, UserHelper } from './';
+import { ApiHelper, UserInterface, RoleMemberInterface, UserHelper, LoadCreateUserRequestInterface } from './';
 import { EnvironmentHelper } from '../../Utils';
 
 interface Props {
@@ -13,10 +13,24 @@ export const UserAdd: React.FC<Props> = (props) => {
     const [email, setEmail] = React.useState("");
 
     const handleSave = () => {
-        const user: UserInterface = { email: email, displayName: email };
-        ApiHelper.apiPost(EnvironmentHelper.AccessManagementApiUrl + '/users/loadOrCreate', user).then(u => {
+        //const user: UserInterface = { email: email, displayName: email }
+
+
+        const resetUrl = window.location.href.replace(window.location.pathname, '') + '/login?auth={auth}';
+        var req: LoadCreateUserRequestInterface = {
+            userName: email,
+            userEmail: email,
+            fromEmail: "support@streaminglive.church",
+            subject: "Welcome to StreamingLive",
+            body: "You have been granted access to " + UserHelper.currentChurch.name + " on StreamingLive. Please click here to set your password: <a href=\"" + resetUrl + "\">" + resetUrl + "</a>"
+        };
+
+
+        ApiHelper.apiPost(EnvironmentHelper.AccessManagementApiUrl + '/users/loadOrCreate', req).then(u => {
             const rm: RoleMemberInterface = { userId: u.id, roleId: props.role.id, churchId: UserHelper.currentChurch.id };
-            ApiHelper.apiPost(EnvironmentHelper.AccessManagementApiUrl + '/rolemembers/', [rm]).then(() => { props.updatedFunction() });
+            ApiHelper.apiPost(EnvironmentHelper.AccessManagementApiUrl + '/rolemembers/', [rm]).then(() => { 
+                props.updatedFunction() 
+            });
         });
     }
 
--- StreamingLiveAdmin/src/Utils/ApiHelper.ts
+++ StreamingLiveAdmin/src/Utils/ApiHelper.ts
@@ -10,6 +10,7 @@ export interface SettingInterface { id?: number, churchId?: number, keyName?: st
 //AccessManagment
 export interface ApplicationInterface { name: string, permissions: RolePermissionInterface[] }
 export interface ChurchInterface { id?: number, name: string, registrationDate?: Date, apps?: ApplicationInterface[] }
+export interface LoadCreateUserRequestInterface { userEmail: string, fromEmail: string, subject: string, body: string, userName:string }
 export interface LoginResponseInterface { user: UserInterface, churches: ChurchInterface[], token: string }
 export interface RegisterInterface { churchName?: string, displayName?: string, email?: string, password?: string }
 export interface RoleInterface { id?: number, churchId?: number, appName?: string, name?: string }
